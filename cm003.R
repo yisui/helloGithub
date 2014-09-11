92e58ef7591ec93587c86597bb05cd0af37a1b01
yisui <yisui@math.ubc.ca>
2014-09-10 22:21:05 -0700

    add files

---
+++ cm003.R
@@ -0,0 +1,36 @@
+x  <- 3*4
+is.vector(x)
+length(x)
+x[2]  <- 100
+x
+x[5]  <- 3
+x
+x[11]
+x[0]
+x  <- 1:4
+x
+x^2
+rnorm(5, mean = 10^(1:5))
+(y  <-  1:3)
+z  <- (3:7)
+z
+y + z
+(y  <- 1:2)
+(z  <- 1:4)
+y * z
+
+x  <- c("hello", "world")
+x
+str(x)
+
+y  <- c(1:3,100,150)
+y
+str(y)
+
+(x  <- c("cabbage", pi, TRUE, 4.3))
+str(x)
+
+str(c(0L, 1L))
+str(c(0,1))
+
+
