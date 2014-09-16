6f28a496d54109fabfc3b51237acaa637d37c4a4
yisui <yisui@math.ubc.ca>
2014-09-15 19:37:21 -0700

    practice R

--- cm003.R
+++ cm003.R
@@ -50,8 +50,48 @@ str(y)
 
 (x  <- c("cabbage", pi, TRUE, 4.3))
 str(x)
+mode(x)
+class(x)
 
 str(c(0L, 1L))
 str(c(0,1))
 
+x <- 1: 4
+(y <- x^2)
+z <- vector (mode = mode (x), length = length(x))
+for (i in seq_along(x)){
+  z[i] <- x[i]^2
+}
+identical(y,z)
 
+(w <- LETTERS[1 : 10])
+
+n <- 8
+set.seed(1)
+(w <- round(rnorm(n),2))
+str(w)
+length(x)
+is.logical(w)
+(z <- runif(n) > 0.3)
+as.numeric(z)
+
+names(w) <- letters[seq_along(w)]
+w
+w < 0
+which (w<0)
+w[w<0]
+w[-c(2,5)]
+
+n <- 8
+set.seed(1)
+(jDat <- data.frame(w = round(rnorm(n), 2),
+                    x = 1:n,
+                    y = I(LETTERS[1:n]),
+                    z = runif(n) > 0.3,
+                    v = rep(LETTERS[9:12], each = 2)))
+str(jDat)
+mode(jDat)
+class(jDat)
+is.list(jDat)
+jDat$v
+str(jDat[c("x", "z")])
